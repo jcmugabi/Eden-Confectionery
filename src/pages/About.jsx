999fec7bfc8a3b6782ea7101e367373d6f61925f
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-02-13 11:07:45 +0300

    contact-header section added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,22 +11,20 @@ function About() {
         </div>
       </header>
       <div className='container my-5'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Deserunt minus laudantium exercitationem corporis, eius soluta sit ex dolorem illum minima similique, 
-        quia qui temporibus saepe quasi est unde magni nam voluptatem earum molestiae fugiat veritatis dignissimos. 
-        Itaque, tempora facilis qui nesciunt veniam ipsum pariatur ut cupiditate quaerat aperiam provident 
-        quis optio fuga molestiae voluptatem velit ullam laboriosam animi est dolorem quidem voluptas 
-        temporibus consectetur sed. Ad repellendus possimus, beatae quo nobis nam eligendi quos. 
-        Inventore sint nobis cupiditate, numquam eius laborum temporibus tenetur dignissimos. 
-        Incidunt fuga cum commodi repudiandae adipisci.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Deserunt minus laudantium exercitationem corporis, eius soluta sit ex dolorem illum minima similique, 
-        quia qui temporibus saepe quasi est unde magni nam voluptatem earum molestiae fugiat veritatis dignissimos. 
-        Itaque, tempora facilis qui nesciunt veniam ipsum pariatur ut cupiditate quaerat aperiam provident 
-        quis optio fuga molestiae voluptatem velit ullam laboriosam animi est dolorem quidem voluptas 
-        temporibus consectetur sed. Ad repellendus possimus, beatae quo nobis nam eligendi quos. 
-        Inventore sint nobis cupiditate, numquam eius laborum temporibus tenetur dignissimos. 
-        Incidunt fuga cum commodi repudiandae adipisci.</p>
+        <p>Located in Seeta Mukono and founded in 2017 by Ms. Joy Kezia Kisakye, 
+          our bakery is a testament to her passion for crafting delectable confections. 
+          At our bakery, we specialize in creating cakes that not only tantalize the taste buds but also serve as stunning centerpieces for any occasion. 
+          From elegant wedding cakes to whimsical birthday creations, we pour our heart and soul into every confection we bake, 
+          ensuring that each bite is a delightful experience for our customers. 
+          With a commitment to quality ingredients and unparalleled attention to detail, 
+          we strive to exceed expectations with every cake that leaves our kitchen.</p>
+        <p>Since our inception, our bakery has become a beloved destination for cake lovers in Kampala and beyond. 
+          With a diverse menu featuring an array of flavors, designs, and sizes, 
+          there's something to satisfy every craving and suit every celebration. 
+          Whether you're marking a milestone birthday, commemorating a special anniversary, 
+          or simply indulging in a sweet treat, our cakes are crafted with care and creativity. 
+          As a family-owned and operated business, we take pride in serving our community with delicious desserts that bring joy to every occasion. 
+          Discover the magic of our bakery and let us elevate your celebrations with our mouthwatering cakes.</p>
       </div>
       <div className='bg-dark text-light'>
         <CakeGallery />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Contact.css'
 
 function Contact() {
   return (
-    <div>
-        <br />
-        <br />
-        <h1 className='at-5'>Contact</h1>
+    <div className='contact-page'>
+        <header className='mt-5'>
+          <div className='container h-100 d-flex align-items-center justify-content-center'>
+            <h1 className='text-light'>Contact</h1>
+          </div>
+        </header>
     </div>
   )
 }
