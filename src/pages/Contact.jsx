4f3271a9d2ef1e92feeaa9412ed4d2784beaf347
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-02-15 15:38:04 +0300

    Contact page complete

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Contact.css'
+import {ContactInfo} from '../components/ContactInfo';
+import {Form, Label} from 'react-bootstrap';
 
 function Contact() {
   return (
@@ -9,6 +11,54 @@ function Contact() {
             <h1 className='text-light'>Contact</h1>
           </div>
         </header>
+        <div className='container my-5'>
+          <div className='row'>
+            <div className='col-lg-6 d-flex align-items-center justify-content-center'>
+              <ContactInfo />
+            </div>
+            <div className='col-lg-6 d-flex justify-content-center'>
+              <Form>
+                <Form.Group className='row mb-3'>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='first-name'>First Name</Form.Label>
+                    <Form.Control type='text' id='first-name' />
+                  </div>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='last-name'>Last Name</Form.Label>
+                    <Form.Control type='text' id='last-name' />
+                  </div>
+                </Form.Group>
+                <Form.Group className='row mb-3'>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='email-address'>Email Adress</Form.Label>
+                    <Form.Control type='email' id='email-address' />
+                  </div>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='phone-number'>Phone Number</Form.Label>
+                    <Form.Control type='tel' id='phone-number' />
+                  </div>
+                </Form.Group>
+                <Form.Group className='row mb-3'>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='date'>Date</Form.Label>
+                    <Form.Control type='date' id='date' />
+                  </div>
+                  <div className='col-md-6'>
+                    <Form.Label htmlFor='cake-type'>Cake Type</Form.Label>
+                    <Form.Control type='text' id='cake-type' />
+                  </div>
+                </Form.Group>
+                <Form.Group className='mb-4'>
+                    <Form.Label htmlFor='comments'>Comments</Form.Label>
+                    <Form.Control type='textarea' id='comments' />
+                </Form.Group>
+
+                <button type='submit' className='btn btn-success btn-lg'>Submit</button>
+              </Form>
+            </div>
+          </div>
+        </div>
+        <div className='bg-dark text-light py-5'>Reviews</div>
     </div>
   )
 }
