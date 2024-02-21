c0f10c7c06ea5935fb9ef9e68da6877b5bbd55cc
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-02-21 10:34:22 +0300

    logic on cakes page changed

---
+++ src/components/Slider.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, Carousel } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Slider = ({ numSlides, imagesAndPrices }) => {
+  return (
+    <Carousel indicators={false} pause={false}>
+      {[...Array(numSlides)].map((_, slideIndex) => (
+        <Carousel.Item key={slideIndex}>
+          <motion.div
+            whileHover={{ scale: 0.98 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Container>
+              <Row className='justify-content-center'>
+                {[0, 1, 2].map((itemIndex) => {
+                  const dataIndex = slideIndex * 3 + itemIndex;
+                  const item = imagesAndPrices[dataIndex];
+                  if (!item) return null; // Skip if no item exists
+                  return (
+                    <Col lg={4} md={12} key={dataIndex}>
+                      <Card className='border-0'>
+                        <Card.Img variant='top' src={item.img} alt='cake' />
+                        <Card.ImgOverlay className='d-flex align-items-start'>
+                          <Card.Text className='text-dark fs-3 fw-bold'>
+                            {item.price}
+                          </Card.Text>
+                        </Card.ImgOverlay>
+                      </Card>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </Container>
+          </motion.div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Slider;
--- src/pages/Cakes.jsx
+++ src/pages/Cakes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Carousel, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+import Slider from '../components/Slider';
 import RedVelvetImg from '../imgs/redvelvet-cake.jpg';
 import LemonImg from '../imgs/lemon-cake.jpg';
 import CaramelImg from '../imgs/caramel-cake.jpg';
@@ -36,38 +37,7 @@ const Cakes = () => {
       <div className='cake-carousel my-5'>
         <Container>
           <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Birthday Cakes</h2>
-          <Carousel indicators={false} pause={false}>
-            {[...Array(numSlides)].map((_, slideIndex) => (
-              <Carousel.Item key={slideIndex}>
-                <motion.div
-                  whileHover={{ scale: 0.98 }}
-                  transition={{ duration: 0.5 }}
-                >
-                  <Container>
-                    <Row className='justify-content-center'>
-                      {[0, 1, 2].map((itemIndex) => {
-                        const dataIndex = slideIndex * 3 + itemIndex;
-                        const item = imagesAndPrices[dataIndex];
-                        if (!item) return null; // Skip if no item exists
-                        return (
-                          <Col lg={4} md={12} key={dataIndex}>
-                            <Card className='border-0'>
-                              <Card.Img variant='top' src={item.img} alt='cake' />
-                              <Card.ImgOverlay className='d-flex align-items-start'>
-                                <Card.Text className='text-dark fs-3 fw-bold'>
-                                  {item.price}
-                                </Card.Text>
-                              </Card.ImgOverlay>
-                            </Card>
-                          </Col>
-                        );
-                      })}
-                    </Row>
-                  </Container>
-                </motion.div>
-              </Carousel.Item>
-            ))}
-          </Carousel>
+          <Slider numSlides={numSlides} imagesAndPrices={imagesAndPrices} />
         </Container>
       </div>
     </div>
