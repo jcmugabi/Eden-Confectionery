f510dc2c6a01b9cdbaf10a7e288c5b373850ee29
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-02-23 12:43:43 +0300

    small changes on the contacts

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,9 +4,9 @@ export function ContactInfo() {
     return(
         <div className="d-flex flex-column align-items-center">
             <h2 className="fs-1 mb-3 text-uppercase fw-bold"> Where To Find Us</h2>
-            <p className="mb-5">Namanve. Jinja Road, Opposite Jomayi Stones.</p>
-            <h6>+256 781 552 058</h6>
-            <h6>+256 701 355 787</h6>
+            <p className="mb-1">Namanve. Jinja Road, Opposite Jomayi Stones.</p>
+            <p className="mb-1 fw-bold fst-italic">+256 781 552 058</p>
+            <p className="mb-4 fw-bold fst-italic">+256 701 355 787</p>
             <h3 >Opening Hours</h3>
             <p>Mon - Fri: 09:00 - 17:00</p>
             <p>Sat - Sun: 10:00 - 15:00</p>
