36787bd43b4de0095efb28d33c6460735a19ad4e
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-03-13 18:10:21 +0300

    sections updated but incomplete

--- src/pages/Cakes.jsx
+++ src/pages/Cakes.jsx
@@ -40,12 +40,91 @@ const Chocolate = [
   // Add more images and prices here
 ];
 
+const Kids = [
+  { img: require('../imgs/gallery/kids/kids-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/kids/kids-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/kids/kids-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/kids/kids-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/kids/kids-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/kids/kids-6.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
+const Fruit = [
+  { img: require('../imgs/gallery/fruit/fruit-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/fruit/fruit-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/fruit/fruit-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/fruit/fruit-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/fruit/fruit-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/fruit/fruit-1.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
+const Birthday = [
+  { img: require('../imgs/gallery/birthday-specials/bd-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/birthday-specials/bd-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/birthday-specials/bd-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/birthday-specials/bd-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/birthday-specials/bd-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/birthday-specials/bd-6.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
+const Weddings = [
+  { img: require('../imgs/gallery/wedding-intro/wed-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/wedding-intro/wed-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/wedding-intro/wed-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/wedding-intro/wed-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/wedding-intro/wed-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/wedding-intro/wed-6.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
+const Graduation = [
+
+  { img: require('../imgs/gallery/graduation/grad-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/graduation/grad-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/graduation/grad-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/graduation/grad-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/graduation/grad-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/graduation/grad-6.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
+const Christmas = [
+  { img: require('../imgs/gallery/christmas/cmas-1.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/christmas/cmas-2.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/christmas/cmas-3.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/christmas/cmas-4.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/christmas/cmas-5.jpg'), price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/christmas/cmas-6.jpg'), price: 'UGX 50,000' },
+  // Add more images and prices here
+];
+
 const Cakes = () => {
   // Calculate number of slides for ButterCream section
   const numSlidesButterCream = Math.ceil(ButterCream.length / 3);
 
   // Calculate number of slides for Chocolate section
   const numSlidesChocolate = Math.ceil(Chocolate.length / 3);
+
+  // Calculate number of slides for Kids section
+  const numSlidesKids = Math.ceil(Kids.length / 3);
+
+  // Calculate number of slides for Fruit section
+  const numSlidesFruit = Math.ceil(Fruit.length / 3);
+  
+  // Calculate number of slides for Birthday Specials section
+  const numSlidesBirthday = Math.ceil(Birthday.length / 3);
+
+  // Calculate number of slides for Weddings and Intros section
+  const numSlidesWeddings = Math.ceil(Weddings.length / 3);
+
+  // Calculate number of slides for Graduation section
+  const numSlidesGraduation = Math.ceil(Graduation.length / 3);
+
+  // Calculate number of slides for Christmas section
+  const numSlidesChristmas = Math.ceil(Christmas.length / 3);
 
   return (
     <div className='menu-page'>
@@ -59,8 +138,27 @@ const Cakes = () => {
         <Container>
           <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>ButterCream Cakes</h2>
           <Slider imagesAndPrices={ButterCream} numSlides={numSlidesButterCream} />
+
           <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Chocolate Cakes</h2>
           <Slider imagesAndPrices={Chocolate} numSlides={numSlidesChocolate} reverseAnimation={true} />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Kids Cakes</h2>
+          <Slider imagesAndPrices={Kids} numSlides={numSlidesKids}  />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Fruit Cakes</h2>
+          <Slider imagesAndPrices={Fruit} numSlides={numSlidesFruit} reverseAnimation={true} />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Birthday Specials</h2>
+          <Slider imagesAndPrices={Birthday} numSlides={numSlidesBirthday}  />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Wedding Cakes</h2>
+          <Slider imagesAndPrices={Weddings} numSlides={numSlidesWeddings} reverseAnimation={true} />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Graduation Cakes</h2>
+          <Slider imagesAndPrices={Graduation} numSlides={numSlidesGraduation}  />
+
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Christmas Cakes</h2>
+          <Slider imagesAndPrices={Christmas} numSlides={numSlidesChristmas} reverseAnimation={true} />
         </Container>
       </div>
     </div>
