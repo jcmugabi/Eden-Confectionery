445f5fd7c75caa755ee789c837ff7a61f9be723a
Joseph Charles Mugabi <josephcharlesmugabi@gmail.com>
2024-03-13 11:46:31 +0300

    Images included, not rendered yet

--- src/pages/Cakes.jsx
+++ src/pages/Cakes.jsx
@@ -22,6 +22,43 @@ const imagesAndPrices = [
   // Add more images and prices here
 ];
 
+const ButterCream = [
+  { img: require('../imgs/gallery/butter-cream/bc-1.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-2.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-3.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-4.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-5.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-6.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-7.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-8.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-9.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-10.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-11.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-12.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/butter-cream/bc-13.jpg').default, price: 'UGX 70,000' }
+];
+
+const Chocolate = [
+  { img: require('../imgs/gallery/chocolate/choc-1.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/chocolate/choc-2.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/chocolate/choc-3.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-4.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/chocolate/choc-5.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/chocolate/choc-6.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-7.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/chocolate/choc-8.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/chocolate/choc-9.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-10.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/chocolate/choc-11.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/chocolate/choc-12.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-13.jpg').default, price: 'UGX 50,000' },
+  { img: require('../imgs/gallery/chocolate/choc-14.jpg').default, price: 'UGX 60,000' },
+  { img: require('../imgs/gallery/chocolate/choc-15.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-16.jpg').default, price: 'UGX 70,000' },
+  { img: require('../imgs/gallery/chocolate/choc-17.jpg').default, price: 'UGX 70,000' },
+];
+
+
 const Cakes = () => {
   // Calculate number of slides
   const numSlides = Math.ceil(imagesAndPrices.length / 3);
@@ -36,10 +73,10 @@ const Cakes = () => {
 
       <div className='cake-carousel my-5'>
         <Container>
-          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Birthday Cakes</h2>
-          <Slider numSlides={numSlides} imagesAndPrices={imagesAndPrices} />
-          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Wedding Cakes</h2>
-          <Slider numSlides={numSlides} imagesAndPrices={imagesAndPrices} reverseAnimation={true} />
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>ButterCream Cakes</h2>
+          <Slider imagesAndPrices={ButterCream} numSlides={numSlides} />
+          <h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>Chocolate Cakes</h2>
+          <Slider imagesAndPrices={Chocolate} numSlides={numSlides} reverseAnimation={true} />
         </Container>
       </div>
     </div>
